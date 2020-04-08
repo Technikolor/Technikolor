c5f73fc5df4afb38423f76d0ecac1f9532c2cbbd
Damian Mąsior <damian.masior@auto1.com>
2020-04-08 23:06:49 +0200

    phpcs and psalm checks

--- app/Book.php
+++ app/Book.php
@@ -20,5 +20,15 @@ class Book
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private string $s;
+    private string $name;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
 }
